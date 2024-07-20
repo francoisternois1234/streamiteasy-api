b2e50479001468bd3c613e85498e4c302744bede
francoisternois1234 <francois.ternois@outlook.com>
2024-07-20 14:46:02 +0200

    chore: Add CORS middleware to allow all origins and methods for the API

--- streamiteasy-api.py
+++ streamiteasy-api.py
@@ -1,21 +1,34 @@
 from dotenv import load_dotenv
 import os
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from livekit import api
 import uvicorn
 
 load_dotenv()
 app = FastAPI()
 
+origins = [
+    "*"
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  # Autorise les origines listées
+    allow_credentials=True,
+    allow_methods=["*"],  # Autorise toutes les méthodes
+    allow_headers=["*"],  # Autorise tous les headers
+)
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.get("/getToken")
-def get_token():
+@app.get("/getToken/")
+def get_token(identity: str):
     token = api.AccessToken(os.getenv("LIVEKIT_API_KEY"), os.getenv("LIVEKIT_API_SECRET")) \
-        .with_identity("user1") \
-        .with_name("User 1") \
+        .with_identity(identity) \
+        .with_name(identity) \
         .with_grants(api.VideoGrants(
             room_join=True,
             room="my-room"
@@ -23,4 +36,4 @@ def get_token():
     return {"token": token.to_jwt()}
 
 if __name__ == "__main__":
-    uvicorn.run('streamiteasy-api:app', host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("streamiteasy-api:app", host="0.0.0.0", port=8000, reload=True, ssl_keyfile="key.pem", ssl_certfile="cert.pem")
